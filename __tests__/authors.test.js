83c9ccc4f228ed0dfa99b3b42843ed41670461fb
Jenna Goldman <jenna.goldman89@gmail.com>
2020-03-25 16:49:31 -0700

    add POST, GET, PATCH, and DELETE routes and tests for books

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -2,8 +2,6 @@ const { getAuthor, getAuthors, getBooks } = require('../db/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
-const Author = require('../lib/models/Author');
-const Book = require('../lib/models/Book');
 
 describe('author routes', () => {
   it('creates an author', () => {
